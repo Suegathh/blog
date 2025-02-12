77936b0151a6467f26c7b124a44ad7acd0868b21
Suegathh <suzzainnegathu@gmail.com>
2025-02-12 15:29:19 +0300

    update profile pic

--- users/models.py
+++ users/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
-    profile_pic = models.ImageField(upload_to="profile_pics", default="default.jpeg")
+    profile_pic = models.ImageField(upload_to="profile_pics", default="profile_pics/default.jpeg")
     
     def __str__(self):
         return f"{self.user.username}-profile"
--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, UpdateUserForm, UpdateUserProfile
 from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def register(request):
@@ -19,4 +20,23 @@ def register(request):
 def logout_user(request):
     logout(request)
     return render(request, "users/logout.html")
-    
+@login_required
+def user_profile(request):
+    if request.method == "POST":
+        p_form = UpdateUserProfile(request.POST, request.FILES, instance=request.user.profile)
+        u_form = UpdateUserForm(request.POST, instance=request.user)
+        
+        if p_form.is_valid and u_form.is_valid:
+            p_form.save()
+            u_form.save()
+            
+            messages.success(request, "Profile Updated Successfully")
+            return redirect("users-profile")
+    else:
+        p_form = UpdateUserProfile(instance=request.user.profile)
+        u_form = UpdateUserForm(instance=request.user)
+    context = {
+        "p_form": p_form,
+        "u_form": u_form
+    }
+    return render(request, "users/profile.html", context)
